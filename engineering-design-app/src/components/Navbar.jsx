7ef24ea40e1cbc138ba077a778b22465d39776a6
Loekstars <91837174+Loekstars@users.noreply.github.com>
2022-09-26 17:06:52 +0200

    Added Nopage
    You will be redirected whenever the page requested does not exist

--- engineering-design-app/src/components/Navbar.jsx
+++ engineering-design-app/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <nav id="navbar" class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded-b-lg dark:bg-gray-900">
         <div class="container flex flex-wrap justify-between items-center mx-auto">
             <a href="/" class="flex items-center">
-                <LightbulbIcon />
+                <LightbulbIcon style={{color: 'gray'}}/>
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Engineering Design</span>
             </a>
             <button id="button-nav" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
--- engineering-design-app/src/pages/NoPage.jsx
+++ engineering-design-app/src/pages/NoPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
 
 const NoPage = () => {
   return (
-    <div>Error 404</div>
+    <div class="text-center pt-16">
+      <PrecisionManufacturingIcon style={{ fontSize: 100 }} />
+      <h1 class="text-9xl">404</h1>
+      <h2 class="text-6xl">Page Not Found</h2>
+    </div>
   )
 }
 
