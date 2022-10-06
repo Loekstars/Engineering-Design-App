a0e275d6daa042703a57e54f7320eb26ea1c5a80
Loekstars <91837174+Loekstars@users.noreply.github.com>
2022-10-06 20:10:43 +0200

    Update tailwind.config.js

--- engineering-design-app/tailwind.config.js
+++ engineering-design-app/tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
       sans: ["Inter", ...defaultTheme.fontFamily.sans],
@@ -18,6 +18,5 @@ module.exports = {
       md: "768px",
       lg: "1024px",
     },
-    plugins: [require("flowbite/plugin")],
   },
 };
