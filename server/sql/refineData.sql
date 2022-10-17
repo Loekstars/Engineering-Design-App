f0abbaf51a8c40a8d3638f2560e8d75d439bec18
Peter de Bruijne <p.j.d.bruijne@student.tue.nl>
2022-10-17 18:50:18 +0200

    Created query to refine data

--- server/sql/refineData.sql
+++ server/sql/refineData.sql
@@ -1,2 +1,27 @@
-SELECT date_time, sensor_index, TRUNC(AVG(sensor_data) OVER(GROUP BY sensor_index ORDER BY date_time ROWS BETWEEN 3 PRECEDING AND CURRENT ROW), 2) AS refined_data
-FROM SensorData
+INSERT INTO refined_data(
+    refined_data.timestamp,
+    refined_data.sensor_id,
+    refined_data.data
+)
+SELECT
+    raw_data.timestamp AS t,
+    raw_data.sensor_id AS s,
+    TRUNC(
+        AVG(sensor_data) OVER(
+        GROUP BY
+            sensor_index
+        ORDER BY
+            date_time ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
+    ),
+    2
+    ) AS d
+FROM
+    raw_data
+WHERE
+    (t, s) NOT IN(
+    SELECT
+        refined_data.timestamp,
+        refined_data.sensor_id
+    FROM
+        refined_data
+)
